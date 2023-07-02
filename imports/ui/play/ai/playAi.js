4b285b87807c582c5c1c5ae8a294903380736b29
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-07-02 23:42:52 +0800

    class aurora, forced moves, enable autoPlay

--- imports/ui/play/ai/playAi.js
+++ imports/ui/play/ai/playAi.js
@@ -6,20 +6,121 @@ import { Template } from 'meteor/templating';
 import './playAi.html';
 import './playAi.css';
 
+class Aurora {
+  constructor(board, game, moveLogics, selfPlay) {
+    this.board = board;
+    this.game = game;
+    this.moveLogics = moveLogics;
+    this.selfPlay = selfPlay;
+    this.autoPlaySpeed = 400;
+    this.play = true;
+  }
+
+  // eslint-disable-next-line consistent-return
+  makeMove() {
+    if (this.game.isCheckmate()) {
+      this.play = false;
+      if (this.game.turn() === 'w') return console.log('Black won');
+      else return console.log('White won');
+    }
+
+    if (
+      this.game.isDraw() ||
+      this.game.isInsufficientMaterial() ||
+      this.game.isStalemate() ||
+      this.game.isThreefoldRepetition()
+    ) {
+      return console.log('Draw');
+    }
+
+    const priorityMoves = [];
+    this.moveLogics.forEach(logic => {
+      let moves;
+      if (logic === 'take') moves = this.moveTake();
+      priorityMoves.push(...moves);
+    });
+
+    let move;
+    if (priorityMoves.length === 1) {
+      move = priorityMoves[0];
+    } else if (priorityMoves.length > 1) {
+      move = priorityMoves[Math.floor(Math.random() * priorityMoves.length)];
+    } else {
+      const randomMoves = this.game.moves();
+      move = randomMoves[Math.floor(Math.random() * randomMoves.length)];
+    }
+
+    this.game.move(move);
+  }
+
+  moveTake() {
+    const moves = this.game.moves();
+    const priorityMoves = [];
+
+    moves.forEach(move => {
+      if (move.includes('x')) priorityMoves.push(move);
+    });
+
+    return priorityMoves;
+  }
+
+  autoPlay(board) {
+    this.makeMove();
+    board.position(this.game.fen());
+    if (this.play) window.setTimeout(() => this.autoPlay(board), this.autoPlaySpeed);
+  }
+}
+
 Template.playai.onRendered(() => {
   let board = null;
   const game = new Chess();
+  const aurora = new Aurora(board, game, ['take'], false);
 
   function onDragStart(source, piece, position, orientation) {}
 
-  function onDrop(source, target, piece, position) {}
+  // eslint-disable-next-line consistent-return
+  function onDrop(source, target, piece, position) {
+    const from = source;
+    const to = target;
 
-  function onSnapEnd() {}
+    try {
+      const move = game.move({ from, to, promotion: 'q' });
+
+      if (move === null) {
+        throw new Error('Mouvement invalide');
+      }
+      // move valide
+    } catch (error) {
+      // move invalide
+
+      return 'snapback';
+    }
+  }
+
+  // eslint-disable-next-line consistent-return
+  function onSnapEnd() {
+    aurora.makeMove();
+    board.position(game.fen());
+    if (game.isCheckmate()) {
+      if (game.turn() === 'w') return console.log('Black won');
+      else return console.log('White won');
+    }
+
+    if (
+      game.isDraw() ||
+      game.isInsufficientMaterial() ||
+      game.isStalemate() ||
+      game.isThreefoldRepetition()
+    ) {
+      console.log('Draw');
+    }
+  }
 
   const config = {
     pieceTheme: '/img/chesspieces/wikipedia/{piece}.png',
     draggable: true,
     position: 'start',
+    promotion: 'q',
     onDragStart,
     onDrop,
     onSnapEnd,
@@ -27,14 +128,5 @@ Template.playai.onRendered(() => {
 
   // eslint-disable-next-line no-undef
   board = Chessboard('myBoard', config);
-
-  function makeRandomMove() {
-    const moves = game.moves();
-    const move = moves[Math.floor(Math.random() * moves.length)];
-    game.move(move);
-    board.position(game.fen());
-    if (!game.isGameOver()) window.setTimeout(makeRandomMove, 250);
-  }
-
-  makeRandomMove();
+  if (aurora.selfPlay) aurora.autoPlay(board);
 });
--- tests/main.js
+++ tests/main.js
@@ -1,19 +1,19 @@
-import assert from "assert";
+import assert from 'assert';
 
-describe("aurorachess", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "aurorachess");
+describe('aurorachess', () => {
+  it('package.json has correct name', async () => {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'aurorachess');
   });
 
   if (Meteor.isClient) {
-    it("client is not server", function () {
+    it('client is not server', () => {
       assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
+    it('server is not client', () => {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
