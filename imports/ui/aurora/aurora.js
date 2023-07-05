ef63de670bacc2abeffaf189ed73eed3ce3b0a5e
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-07-05 19:28:54 +0800

    optimisation nul : 3 ply > 2000ms

--- imports/ui/aurora/aurora.js
+++ imports/ui/aurora/aurora.js
@@ -38,57 +38,91 @@ export class Aurora {
   }
 
   searchMoves(depth) {
-    const search = [];
-    const engine = new Chess(this.game.fen());
-    let levels = [];
+    let moves = [];
+    const gameFen = this.game.fen();
+    const engine = new Chess(gameFen);
 
-    for (let i = 0; i < depth; i++) {
-      if (i === 0) {
-        engine.load(this.game.fen());
-        // eslint-disable-next-line no-loop-func
-        engine.moves().forEach(move => {
-          engine.load(this.game.fen());
-          engine.move(move);
-          const push = {
-            move,
-            score: this.getScore(engine.fen(), this.color),
-            fen: engine.fen(),
-          };
-          if (depth > 1) push.moves = [];
-          search.push(push);
-          levels.push(push);
+    engine.moves().forEach(move => {
+      engine.load(gameFen);
+      engine.move(move);
+      moves.push({ moves: [move], scores: [this.getScore(engine.fen())] });
+    });
+
+    for (let i = 1; i < depth; i++) {
+      const newMoves = [];
+      moves.forEach(move => {
+        engine.load(gameFen);
+        for (let j = 0; j < move.moves.length; j++) {
+          engine.move(move.moves[j]);
+        }
+
+        const fen = engine.fen();
+        engine.moves().forEach(nextMove => {
+          engine.load(fen);
+          engine.move(nextMove);
+          const push = { ...move };
+          push.moves = move.moves.slice();
+          push.scores = move.scores.slice();
+          push.moves.push(nextMove);
+          push.scores.push(this.getScore(engine.fen()));
+          newMoves.push(push);
         });
-      } else {
-        // eslint-disable-next-line no-loop-func
-        const nextLevels = [];
-        levels.forEach(levelMove => {
-          engine.load(levelMove.fen);
-          engine.moves().forEach(move => {
-            engine.load(levelMove.fen);
-            engine.move(move);
-            const push = {
-              move,
-              score: this.getScore(engine.fen(), this.color),
-              fen: engine.fen(),
-            };
-            if (i !== depth - 1) push.moves = [];
-            levelMove.moves.push(push);
-            nextLevels.push(push);
-          });
-        });
-        levels = nextLevels;
-      }
+      });
+      moves = newMoves;
     }
 
-    return {
-      search,
-      length: levels.length,
-    };
+    return moves;
+
+    // ! VERSION 1 ! 1 ply : 95ms, 2 ply : 408ms, 3 ply : > 2000ms
+    // const search = [];
+    // const engine = new Chess(this.game.fen());
+    // let levels = [];
+    // for (let i = 0; i < depth; i++) {
+    //   if (i === 0) {
+    //     engine.load(this.game.fen());
+    //     // eslint-disable-next-line no-loop-func
+    //     engine.moves().forEach(move => {
+    //       engine.load(this.game.fen());
+    //       engine.move(move);
+    //       const push = {
+    //         move,
+    //         score: this.getScore(engine.fen(), this.color),
+    //         fen: engine.fen(),
+    //       };
+    //       if (depth > 1) push.moves = [];
+    //       search.push(push);
+    //       levels.push(push);
+    //     });
+    //   } else {
+    //     // eslint-disable-next-line no-loop-func
+    //     const nextLevels = [];
+    //     levels.forEach(levelMove => {
+    //       engine.load(levelMove.fen);
+    //       engine.moves().forEach(move => {
+    //         engine.load(levelMove.fen);
+    //         engine.move(move);
+    //         const push = {
+    //           move,
+    //           score: this.getScore(engine.fen(), this.color),
+    //           fen: engine.fen(),
+    //         };
+    //         if (i !== depth - 1) push.moves = [];
+    //         levelMove.moves.push(push);
+    //         nextLevels.push(push);
+    //       });
+    //     });
+    //     levels = nextLevels;
+    //   }
+    // }
+    // return {
+    //   search,
+    //   length: levels.length,
+    // };
   }
 
-  getScore(fen, color) {
+  getScore(fen) {
     const evaluation = new Chess(fen);
-    evaluation._turn = color;
+    evaluation._turn = this.color;
     const board = evaluation.board();
     let score = 0;
     let oppScore = 0;
@@ -96,7 +130,7 @@ export class Aurora {
     // score by pieces
     board.forEach(row => {
       row.forEach(square => {
-        if (square && square.color === color) {
+        if (square && square.color === this.color) {
           score += this.piecesScore[square.type];
         } else if (square) {
           oppScore += this.piecesScore[square.type];
@@ -106,14 +140,14 @@ export class Aurora {
 
     // score by mobility | number of moves
     const oppEvaluation = new Chess(fen);
-    oppEvaluation._turn = color === 'w' ? 'b' : 'w';
+    oppEvaluation._turn = this.color === 'w' ? 'b' : 'w';
 
     score += evaluation.moves().length * this.piecesScore.m;
     oppScore += oppEvaluation.moves().length * this.piecesScore.m;
 
     score -= oppScore;
     score = score.toFixed(3);
-    return `${score} for ${color}`;
+    return score;
   }
 
   autoPlay(board) {
--- tests/moves.test.js
+++ tests/moves.test.js
@@ -11,35 +11,35 @@ describe('Aurora Search : Start Position', function () {
   it('Depth : 1 ply : 20 positions', function () {
     const depth = 1;
 
-    const moves = aurora.searchMoves(depth).length;
-    assert.strictEqual(moves, 20);
+    const moves = aurora.searchMoves(depth);
+    assert.strictEqual(moves.length, 20);
   });
 
   it('Depth : 2 ply : 400 positions', function () {
     const depth = 2;
 
-    const moves = aurora.searchMoves(depth).length;
-    assert.strictEqual(moves, 400);
+    const moves = aurora.searchMoves(depth);
+    assert.strictEqual(moves.length, 400);
   });
 
   it('Depth : 3 ply : 8902 positions', function () {
     const depth = 3;
 
-    const moves = aurora.searchMoves(depth).length;
-    assert.strictEqual(moves, 8902);
+    const moves = aurora.searchMoves(depth);
+    assert.strictEqual(moves.length, 8902);
   });
 
   // it('Depth : 4 ply : 197281 positions', function () {
   //   const depth = 4;
 
-  //   const moves = aurora.searchMoves(depth).length;
-  //   assert.strictEqual(moves, 197281);
+  //   const moves = aurora.searchMoves(depth);
+  //   assert.strictEqual(moves.length, 197281);
   // });
 
   // it('Depth : 5 ply : 4865609 positions', function () {
   //   const depth = 5;
 
-  //   const moves = aurora.searchMoves(depth).length;
-  //   assert.strictEqual(moves, 4865609);
+  //   const moves = aurora.searchMoves(depth);
+  //   assert.strictEqual(moves.length, 4865609);
   // });
 });
