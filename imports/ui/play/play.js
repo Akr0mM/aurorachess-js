f090c706e62765e3742576bf710e4c4216d85660
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-07-30 16:00:26 +0800

    remove old aurora

--- imports/ui/play/play.js
+++ imports/ui/play/play.js
@@ -20,87 +20,28 @@ $(document).on('keydown keyup', event => {
 Template.play.onRendered(() => {
   let board = null;
   let aurora = null;
-  // const game = new Chess();
-  // const aurora = new Aurora(board, game, 'b', false);
-  // $('#depth-input').attr('max', aurora.MAX_DEPTH);
-  // $('#depth-input').val(aurora.DEFAULT_DEPTH);
 
-  function onDragStart(source, piece) {
-    aurora.highlightMoves(source, piece);
-  }
+  function onDragStart(source, piece) {}
 
   // eslint-disable-next-line consistent-return
   function onDrop(source, target, piece) {
-    $('.highlight-moves-white').removeClass('highlight-moves-white');
-    $('.highlight-moves-black').removeClass('highlight-moves-black');
-    $('.highlight-moves-source-white').removeClass(
-      'highlight-moves-source-white',
-    );
-    $('.highlight-moves-source-black').removeClass(
-      'highlight-moves-source-black',
-    );
-
-    let promotion = 'q';
-
-    if (
-      shiftKey &&
-      piece[1] === 'P' &&
-      (parseInt(target[1], 10) === 1 || parseInt(target[1], 10) === 8)
-    ) {
-      console.log(piece[1], target[1]);
-      promotion = '';
-      while (
-        promotion !== 'q' &&
-        promotion !== 'n' &&
-        promotion !== 'r' &&
-        promotion !== 'b'
-      ) {
-        promotion = prompt(
-          'Promotion: Queen (q), Knight (n), Rook (r), Bishop (b)',
-        );
-      }
-      shiftKey = false;
-    }
-
-    const move = aurora.isMove(source, target, promotion);
-
-    if (move) {
-      aurora.playMove(move);
-      if (move.split(' ')[3]) board.position(aurora.getFEN());
-    } else return 'snapback';
+    // $('.highlight-moves-white').removeClass('highlight-moves-white');
+    // $('.highlight-moves-black').removeClass('highlight-moves-black');
+    // $('.highlight-moves-source-white').removeClass(
+    //   'highlight-moves-source-white',
+    // );
+    // $('.highlight-moves-source-black').removeClass(
+    //   'highlight-moves-source-black',
+    // );
   }
 
   // eslint-disable-next-line consistent-return
-  function onSnapEnd() {
-    if (aurora.updateBoardOnSnapEnd) board.position(aurora.getFEN());
-    // const moves = aurora.getMoves();
-    // const move = moves[Math.floor(Math.random() * moves.length)];
-    // aurora.playMove(move);
-
-    // $('#evaluation').text(aurora.evaluatePosition(game.fen()));
-    // const depthInput = parseInt($('#depth-input').val(), 10);
-    // aurora.makeMove(depthInput);
-    // $('#evaluation').text(aurora.evaluatePosition(game.fen()));
-    // if (game.isCheckmate()) {
-    //   if (game.turn() === 'w') return console.log('Black won');
-    //   else return console.log('White won');
-    // }
-    // if (
-    //   game.isDraw() ||
-    //   game.isInsufficientMaterial() ||
-    //   game.isStalemate() ||
-    //   game.isThreefoldRepetition()
-    // ) {
-    //   console.log('Draw');
-    // }
-    // board.position(aurora.getFEN());
-  }
+  function onSnapEnd() {}
 
   const boardConfig = {
     pieceTheme: '/chesspieces/neo/{piece}.png',
     draggable: true,
     position: fen,
-    moveSpeed: 0,
     onDragStart,
     onDrop,
     onSnapEnd,
--- tests/moves.test.js
+++ tests/moves.test.js
@@ -1,34 +1,35 @@
 /* eslint-disable no-undef */
 /* eslint-disable prefer-arrow-callback */
-import { Chess } from 'chess.js';
 import { assert } from 'chai';
 import { Aurora } from '../imports/ui/aurora/aurora';
 
 describe('Aurora Search : Start Position', function () {
-  const game = new Chess();
-  const aurora = new Aurora(null, game, 'w', false);
+  const config = {
+    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+  };
+  const aurora = new Aurora(config);
 
   it('Depth : 1 ply : 20 positions', function () {
     const depth = 1;
 
-    const moves = aurora.searchMoves(depth);
+    const moves = aurora.getMoves(depth);
     assert.strictEqual(moves.length, 20);
   });
 
-  it('Depth : 2 ply : 400 positions', function () {
-    const depth = 2;
+  // it('Depth : 2 ply : 400 positions', function () {
+  //   const depth = 2;
 
-    const moves = aurora.searchMoves(depth);
-    assert.strictEqual(moves.length, 400);
-  });
+  //   const moves = aurora.searchMoves(depth);
+  //   assert.strictEqual(moves.length, 400);
+  // });
 
-  it('Depth : 3 ply : 8902 positions', function () {
-    this.timeout(0);
-    const depth = 3;
+  // it('Depth : 3 ply : 8902 positions', function () {
+  //   this.timeout(0);
+  //   const depth = 3;
 
-    const moves = aurora.searchMoves(depth);
-    assert.strictEqual(moves.length, 8902);
-  });
+  //   const moves = aurora.searchMoves(depth);
+  //   assert.strictEqual(moves.length, 8902);
+  // });
 
   // it('Depth : 4 ply : 197281 positions', function () {
   //   const depth = 4;
