f631639625f189a71c759cd6a9f2d54eff9fb840
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2023-08-10 18:17:53 +0200

    update tests

--- tests/moves.test.js
+++ tests/moves.test.js
@@ -3,45 +3,56 @@
 import { assert } from 'chai';
 import { Aurora } from '../imports/ui/aurora/aurora';
 
-describe('Aurora Search : Start Position', function () {
-  const config = {
-    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-  };
-  const aurora = new Aurora(config);
+const fen = {
+  start: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+  random:
+    'kQqNrBPR/rRbnQbQp/RbQQkPNQ/pBnRPqPp/QbpkKNKK/NqKBpBrp/pkRqnkKp/KrBpKRqr w - - 0 1',
+};
 
-  it('Depth : 1 ply : 20 positions', function () {
-    const depth = 1;
+describe('Aurora Bitboards Initialization ', function () {
+  let aurora;
 
-    const moves = aurora.getMoves(depth);
-    assert.strictEqual(moves.length, 20);
+  it('give all the bitboards from starting position', function () {
+    aurora = new Aurora({ fen: fen.start });
+    assert.strictEqual(aurora.wp.toString(16), 'ff00'); // bitboard des pions blanc a la position de depart
+    assert.strictEqual(aurora.wr.toString(16), '81'); // bitboard des tours blanche a la position de depart
+    assert.strictEqual(aurora.wn.toString(16), '42'); // bitboard des cavaliers blanc a la position de depart
+    assert.strictEqual(aurora.wb.toString(16), '24'); // bitboard des fous blanc a la position de depart
+    assert.strictEqual(aurora.wq.toString(16), '10'); // bitboard des dames blanche a la position de depart
+    assert.strictEqual(aurora.wk.toString(16), '8'); // bitboard des rois blanc a la position de depart
+    assert.strictEqual(aurora.bp.toString(16), 'ff000000000000'); // bitboard des pions noir a la position de depart
+    assert.strictEqual(aurora.br.toString(16), '8100000000000000'); // bitboard des tours noire a la position de depart
+    assert.strictEqual(aurora.bn.toString(16), '4200000000000000'); // bitboard des cavaliers noir a la position de depart
+    assert.strictEqual(aurora.bb.toString(16), '2400000000000000'); // bitboard des fous noir a la position de depart
+    assert.strictEqual(aurora.bq.toString(16), '1000000000000000'); // bitboard des dames noire a la position de depart
+    assert.strictEqual(aurora.bk.toString(16), '800000000000000'); // bitboard des rois noir a la position de depart
   });
 
-  // it('Depth : 2 ply : 400 positions', function () {
-  //   const depth = 2;
+  it('give all the bitboards from random position', function () {
+    aurora = new Aurora({ fen: fen.random });
+    assert.strictEqual(aurora.wp.toString(16), '200040a00000000'); // bitboard des pions blanc a la position random
+    assert.strictEqual(aurora.wr.toString(16), '140801000002004'); // bitboard des tours blanche a la position random
+    assert.strictEqual(aurora.wn.toString(16), '1000020004800000'); // bitboard des cavaliers blanc a la position random
+    assert.strictEqual(aurora.wb.toString(16), '400004000140020'); // bitboard des fous blanc a la position random
+    assert.strictEqual(aurora.wq.toString(16), '400a310080000000'); // bitboard des dames blanche a la position random
+    assert.strictEqual(aurora.wk.toString(16), 'b200288'); // bitboard des rois blanc a la position random
+    assert.strictEqual(aurora.bp.toString(16), '1008120098110'); // bitboard des pions noir a la position random
+    assert.strictEqual(aurora.br.toString(16), '880000000020041'); // bitboard des tours noire a la position random
+    assert.strictEqual(aurora.bn.toString(16), '10002000000800'); // bitboard des cavaliers noir a la position random
+    assert.strictEqual(aurora.bb.toString(16), '24400040000000'); // bitboard des fous noir a la position random
+    assert.strictEqual(aurora.bq.toString(16), '2000000400401002'); // bitboard des dames noire a la position random
+    assert.strictEqual(aurora.bk.toString(16), '8000080010004400'); // bitboard des rois noir a la position random
+  });
+});
 
-  //   const moves = aurora.searchMoves(depth);
-  //   assert.strictEqual(moves.length, 400);
-  // });
+describe('Aurora Get Moves', function () {
+  let aurora;
 
-  // it('Depth : 3 ply : 8902 positions', function () {
-  //   this.timeout(0);
-  //   const depth = 3;
+  it('should give 20 moves from the starting position', function () {
+    aurora = new Aurora({ fen: fen.start });
 
-  //   const moves = aurora.searchMoves(depth);
-  //   assert.strictEqual(moves.length, 8902);
-  // });
+    const moves = aurora.getMoves();
 
-  // it('Depth : 4 ply : 197281 positions', function () {
-  //   const depth = 4;
-
-  //   const moves = aurora.searchMoves(depth);
-  //   assert.strictEqual(moves.length, 197281);
-  // });
-
-  // it('Depth : 5 ply : 4865609 positions', function () {
-  //   const depth = 5;
-
-  //   const moves = aurora.searchMoves(depth);
-  //   assert.strictEqual(moves.length, 4865609);
-  // });
+    assert.strictEqual(moves.length, 16);
+  });
 });
